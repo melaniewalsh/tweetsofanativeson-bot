2185de7ad3e8d080a962dbecfcae17b3dfbb8dc2
Melanie Walsh <melanie.walsh.wustl.edu>
2018-07-08 16:49:48 -0500

    add keys and code

---
+++ tweetsofanativeson.py
@@ -0,0 +1,12 @@
+from twython import Twython, TwythonError
+from secrets import *
+
+twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
+
+#Setting Twitter's search results as a variable
+search_results = twitter.search(q="james baldwin", count=1, results_type='popular')
+try:
+    for tweet in search_results["statuses"]:
+        twitter.retweet(id = tweet["id_str"])
+except TwythonError as e:
+    print e
