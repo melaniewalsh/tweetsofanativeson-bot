6154bdb294b02d906e719062f35d4ab2a6923561
Melanie Walsh <melanie.walsh.wustl.edu>
2018-07-09 09:04:33 -0500

    untrack secrets

--- secrets.py
+++
@@ -1,4 +0,0 @@
-app_key = "qUjaffkMPhXkYjPJAy71ftYby"
-app_secret = "PRIBCrjC7T9ABb4mM2fnb3tFxkmDcfp76Mx0JCoiMRPC5HCy8T"
-oauth_token = "1016074768855027713-7R4bEMTqyoBaiGfG9D0VRYFQgNMvxy"
-oauth_token_secret = "dzofeqE4rqYvLJdrmlSWCIBfacCaBHfgwwka1oXWXjD8h"
