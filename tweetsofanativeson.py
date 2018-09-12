e628f296e68b23128beeaee3ce77ea058c82b7ea
Melanie Walsh <melanie.walsh.wustl.edu>
2018-09-12 10:01:03 -0500

    limit search results to exact phrase and expand beyond popular

--- tweetsofanativeson.py
+++ tweetsofanativeson.py
@@ -4,13 +4,17 @@ from secrets import *
 twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 
 #Setting Twitter's search results as a variable
-search_results = twitter.search(q="james baldwin", count=100, result_type="popular")
+search_results = twitter.search(q="'james baldwin'", count=100)
+
 
 tweet_counter=0
 
 for tweet in search_results["statuses"]:
     try:
-        if tweet['retweet_count'] > 100 or tweet['user']['followers_count'] > 10000 and tweet_counter ==0:
+        if tweet['retweet_count'] > 100 and tweet_counter ==0:
+            twitter.retweet(id = tweet["id_str"])
+            tweet_counter +=1
+        elif tweet['user']['followers_count'] > 10000 and tweet_counter ==0:
             twitter.retweet(id = tweet["id_str"])
             tweet_counter +=1
     except TwythonError as e:
